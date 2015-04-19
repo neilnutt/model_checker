f174e066572a7667b05ac8c29339e8098f00de15
neilnutt <neilnutt@googlemail.com>
2015-04-19 22:25:07 +0100

    Ability to edit .ief and messages field
    Introduction of functionality to edit the .ief using a text editor
    Added messages field for storing errors or other messages

--- isisRun.py
+++ isisRun.py
@@ -24,7 +24,7 @@ Front page tab for calculation, used to hold catchment title, author and checkin
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 '''
-import wx,time,os
+import wx,time,os,subprocess
 
 class Fpanel(wx.Panel):
     def __init__(self, parent,p):
@@ -32,57 +32,124 @@ class Fpanel(wx.Panel):
         self.p=p
         self.dirName = ''
         self.fileName = ''
+        self.iefEditor = 'C:\\Program Files (x86)\\Notepad++\\notepad++.exe'
 
         self.iefFileLabel = wx.StaticText(self, -1, ".ief file" ) 
         self.datFileLabel = wx.StaticText(self, -1, ".dat file" )  
         self.iedFileLabel = wx.StaticText(self, -1, ".ied file") 
         self.resultsDirLabel= wx.StaticText(self, -1, "Results dir")
         self.icLabel = wx.StaticText(self, -1, "Initial conditions") 
-        self.tcfFileLabel = wx.StaticText(self, -1, ".tcf file")      
-
-        self.iefFile = wx.TextCtrl(self, -1, "-",size=(800,25))
-        self.datFile = wx.TextCtrl(self, -1, "-",size=(800,25), style =wx.TE_READONLY)
-        self.iedFile = wx.TextCtrl(self, -1, "-",size=(800,25), style =wx.TE_READONLY)
-        self.resultsDir = wx.TextCtrl(self, -1, "-",size=(800,25), style =wx.TE_READONLY)
-        self.ic = wx.TextCtrl(self, -1, "-",size=(800,25), style =wx.TE_READONLY)
-        self.tcfFile = wx.TextCtrl(self, -1, "-",size=(800,25), style =wx.TE_READONLY)
+        self.tcfFileLabel = wx.StaticText(self, -1, ".tcf file")
         
+        self.runTypeLabel = wx.StaticText(self, -1, "Run type" ) 
+        self.startLabel = wx.StaticText(self, -1, "Start time" ) 
+        self.finishLabel = wx.StaticText(self, -1, "Finish end" ) 
+        self.timestepLabel = wx.StaticText(self, -1, "1D timestep" ) 
+        self.timestep2DLabel = wx.StaticText(self, -1, "2D timestep" )
+        
+        self.zznFileLabel = wx.StaticText(self, -1, ".zzn file" )
+        self.lastEditTime1DLabel = wx.StaticText(self, -1, "1D last edit" )
+        self.zznTimeLabel = wx.StaticText(self, -1, ".zzn write time" )
+        self.zznSizeLabel = wx.StaticText(self, -1, ".zzn size" )
+        self.zzdMessageLabel = wx.StaticText(self, -1, ".zzd message" )       
+
+        self.iefFile = wx.TextCtrl(self, -1, "-",size=(800,20))
+        self.datFile = wx.TextCtrl(self, -1, "-",size=(800,20), style =wx.TE_READONLY)
+        self.iedFile = wx.TextCtrl(self, -1, "-",size=(800,20), style =wx.TE_READONLY)
+        self.resultsDir = wx.TextCtrl(self, -1, "-",size=(800,20), style =wx.TE_READONLY)
+        self.ic = wx.TextCtrl(self, -1, "-",size=(800,20), style =wx.TE_READONLY)
+        self.tcfFile = wx.TextCtrl(self, -1, "-",size=(800,20), style =wx.TE_READONLY)
+        self.zznFile = wx.TextCtrl(self, -1, "-",size=(800,20), style =wx.TE_READONLY)        
+ 
+        self.iefFileMsg = wx.TextCtrl(self, -1, "-",size=(100,20))
+        self.datFileMsg = wx.TextCtrl(self, -1, "-",size=(100,20), style =wx.TE_READONLY)
+        self.iedFileMsg = wx.TextCtrl(self, -1, "-",size=(100,20), style =wx.TE_READONLY)
+        self.resultsDirMsg = wx.TextCtrl(self, -1, "-",size=(100,20), style =wx.TE_READONLY)
+        self.icMsg = wx.TextCtrl(self, -1, "-",size=(100,20), style =wx.TE_READONLY)
+        self.tcfFileMsg = wx.TextCtrl(self, -1, "-",size=(100,20), style =wx.TE_READONLY)
+        
+        self.runType = wx.TextCtrl(self, -1, "-",size=(400,20), style =wx.TE_READONLY)
+        self.start = wx.TextCtrl(self, -1, "-",size=(400,20), style =wx.TE_READONLY)
+        self.finish = wx.TextCtrl(self, -1, "-",size=(400,20), style =wx.TE_READONLY)
+        self.timestep = wx.TextCtrl(self, -1, "-",size=(400,20), style =wx.TE_READONLY)
+        self.timestep2D = wx.TextCtrl(self, -1, "-",size=(400,20), style =wx.TE_READONLY) 
+
+        self.lastEditTime1D = wx.TextCtrl(self, -1, "-",size=(400,20), style =wx.TE_READONLY)
+        self.zznTime = wx.TextCtrl(self, -1, "-",size=(400,20), style =wx.TE_READONLY)
+        self.zznSize = wx.TextCtrl(self, -1, "-",size=(400,20), style =wx.TE_READONLY)
+        self.zzdMessage = wx.TextCtrl(self, -1, "-",size=(400,20), style =wx.TE_READONLY)
           
         self.iefFile_btn = wx.Button(self, -1, '...')
-        #self.datFile_btn = wx.Button(self, -1, '...')
-        #self.iedFile_btn = wx.Button(self, -1, '...')
-        #self.resltsDir_btn = wx.Button(self, -1, '...')
-        #self.ic_btn  = wx.Button(self, -1, '...')
-        #self.tfc_btn = wx.Button(self, -1, '...')
+        self.iefFileEdit_btn = wx.Button(self, -1, 'Edit') 
         
         #  Assign actions to buttons
         self.iefFile_btn.Bind(wx.EVT_BUTTON, self.changeIef)
+        self.iefFileEdit_btn.Bind(wx.EVT_BUTTON, self.editIef)
 
         
         # use gridbagsizer for layout of widgets
-        sizer = wx.GridBagSizer(vgap=10, hgap=10)
+        sizer = wx.GridBagSizer(vgap=2, hgap=10)
         
-        sizer.Add(self.iefFileLabel,pos=(0,0),span=(1,1))
+        sizer.Add(self.iefFileLabel,pos=(0,0))
         sizer.Add(self.datFileLabel,pos=(1,0))
         sizer.Add(self.iedFileLabel,pos=(2,0))
         sizer.Add(self.resultsDirLabel,pos=(3,0))
         sizer.Add(self.icLabel,pos=(4,0))
         sizer.Add(self.tcfFileLabel,pos=(5,0))
+        sizer.Add(self.zznFileLabel,pos=(6,0)) 
         
-        sizer.Add(self.iefFile,pos=(0,1),span=(1,1))
-        sizer.Add(self.datFile,pos=(1,1))
-        sizer.Add(self.iedFile,pos=(2,1))
-        sizer.Add(self.resultsDir,pos=(3,1))
-        sizer.Add(self.ic,pos=(4,1))
-        sizer.Add(self.tcfFile,pos=(5,1))
+        sizer.Add(self.runTypeLabel,pos=(8,0))
+        sizer.Add(self.startLabel,pos=(9,0))
+        sizer.Add(self.finishLabel,pos=(10,0))
+        sizer.Add(self.timestepLabel,pos=(11,0))
+        sizer.Add(self.timestep2DLabel,pos=(12,0))
+        
+        sizer.Add(self.zzdMessageLabel,pos=(14,0))
+        sizer.Add(self.lastEditTime1DLabel,pos=(15,0))
+        sizer.Add(self.zznTimeLabel,pos=(16,0))
+        sizer.Add(self.zznSizeLabel,pos=(17,0))       
+        
+        sizer.Add(self.iefFile,pos=(0,1),span=(1,2))
+        sizer.Add(self.datFile,pos=(1,1),span=(1,2))
+        sizer.Add(self.iedFile,pos=(2,1),span=(1,2))
+        sizer.Add(self.resultsDir,pos=(3,1),span=(1,2))
+        sizer.Add(self.ic,pos=(4,1),span=(1,2))
+        sizer.Add(self.tcfFile,pos=(5,1),span=(1,2))
+        sizer.Add(self.zznFile,pos=(6,1))      
+ 
+        sizer.Add(self.iefFileMsg,pos=(0,3))  
+        sizer.Add(self.datFileMsg,pos=(1,3))  
+        sizer.Add(self.iedFileMsg,pos=(2,3))  
+        sizer.Add(self.resultsDirMsg,pos=(3,3))  
+        sizer.Add(self.icMsg,pos=(4,3))  
+        sizer.Add(self.tcfFileMsg,pos=(5,3))        
+        
+        sizer.Add(self.runType,pos=(8,1))
+        sizer.Add(self.start,pos=(9,1))
+        sizer.Add(self.finish,pos=(10,1))
+        sizer.Add(self.timestep,pos=(11,1))
+        sizer.Add(self.timestep2D,pos=(12,1))           
+
+        sizer.Add(self.zzdMessage,pos=(14,1))
+        sizer.Add(self.lastEditTime1D,pos=(15,1))
+        sizer.Add(self.zznTime,pos=(16,1))
+        sizer.Add(self.zznSize,pos=(17,1))        
     
-        sizer.Add(self.iefFile_btn,pos=(0,2))
+        sizer.Add(self.iefFileEdit_btn,pos=(0,4))
+    
+        sizer.Add(self.iefFile_btn,pos=(0,5))
                 
         # use boxsizer to add border around sizer
         border = wx.BoxSizer()
         border.Add(sizer, 0, wx.ALL, 20)
         self.SetSizerAndFit(border)
         self.Fit()
+    
+    def editIef(self,event):
+      if os.path.isfile(self.p.ief.iefFile):
+        subprocess.call([self.iefEditor,self.p.ief.iefFile])
+      else:
+        self.iefFileMsg.SetValue('File not found')
         
     def changeIef(self,event):
       """ File|Open event - Open dialog box. """
@@ -101,7 +168,25 @@ class Fpanel(wx.Panel):
       self.p.ief.checkIef(self.fname)
       self.datFile.SetValue(self.p.ief.datFile)
       self.iedFile.SetValue(self.p.ief.iedFile)
-      self.ic.SetValue(self.p.ief.iCsFrom)
+      self.ic.SetValue(self.p.ief.icFile)
       self.resultsDir.SetValue(self.p.ief.resultLocation)
       self.tcfFile.SetValue(self.p.ief.tcfFile)
       
+      self.runType.SetValue(self.p.ief.runType)
+      self.start.SetValue(self.p.ief.start)
+      self.finish.SetValue(self.p.ief.finish)
+      self.timestep.SetValue(self.p.ief.timestep)
+      self.timestep2D.SetValue(self.p.ief.timestep2D)
+  
+      self.zznFile.SetValue(self.p.ief.zznFile)
+      self.lastEditTime1D.SetValue(str(time.ctime(self.p.ief.lastEditTime1D)))
+      self.zznTime.SetValue(str(time.ctime(self.p.ief.zznTime)))
+      self.zznSize.SetValue(self.p.ief.zznSize)
+      self.zzdMessage.SetValue(self.p.ief.zzdMessage) 
+        
+      self.iefFileMsg.SetValue(self.p.ief.iefFileMsg)
+      self.datFileMsg.SetValue(self.p.ief.datFileMsg)
+      self.iedFileMsg.SetValue(self.p.ief.iedFileMsg)
+      self.resultsDirMsg.SetValue(self.p.ief.resultsDirMsg)
+      self.icMsg.SetValue(self.p.ief.icMsg)
+      self.tcfFileMsg.SetValue(self.p.ief.tcfFileMsg)                  
